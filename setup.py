1bc36e1afff018be4690ac582741b32ab80537cc
David A. Hannasch <David.A.Hannasch@gmail.com>
2019-11-25 10:30:03 -0700

    package_dir

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 from setuptools import setup, find_packages
+import pip
 
+raise Exception(pip.__version__)
 setup(name='funniest',
       version='0.1',
       description='The funniest joke in the world',
@@ -8,4 +10,5 @@ setup(name='funniest',
       author_email='flyingcircus@example.com',
       license='MIT',
       packages=find_packages('src'),
+      package_dir={'': 'src'},
       zip_safe=False)
